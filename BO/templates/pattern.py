e8b54d3c79c0d4966c14b2ca6488602d6de26d5c
Tym Rabchuk <tymrabchuk@gmail.com>
2019-07-02 21:14:15 -0400

    Create pattern.py

---
+++ BO/templates/pattern.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import socket
+import struct
+
+#set up the IP and port to connect to
+RHOST = "192.168.1.238" 
+RPORT = 110
+
+#set up the IP and port to connect to
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((RHOST,RPORT))
+
+data = s.recv(1024)
+
+#build a message
+
+buf = ""
+buf += "PATTERN"
+buf += "\n"
+
+#send the message
+s.send("VALUE" + buf)
+
+#receive some data from socket
+data = s.recv(1024)
+
+#print out what received
+print "Received: {0}".format(data)
